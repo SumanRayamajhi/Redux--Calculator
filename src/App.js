148211fc953cc925a10e519589cbeeb30ce7b4f9
Suman Rayamajhi <meh_sams@yahoo.com>
2022-05-28 23:22:12 +0200

    make some changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import "./App.css";
-//import { Providor } from "react-redux";
 import Calculator from "./calculator/Calculator";
 import { store } from "../src/reducers/store";
 import { Provider } from "react-redux";
@@ -7,9 +6,6 @@ import React from "react";
 
 function App() {
   return (
-    // <div className="App">
-    // </div>
-
     <React.Fragment>
       <Provider store={store}>
         <Calculator />
